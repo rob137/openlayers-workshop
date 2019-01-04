84c574871804248aa82f0fb6bc85205353528b08
rob <robertaxelkirby@gmail.com>
2019-01-04 11:54:19 +0000

    Allow drag and drop of GeoJSON

--- main.js
+++ main.js
@@ -5,21 +5,23 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 import sync from 'ol-hashed';
+import DragAndDrop from 'ol/interaction/DragAndDrop';
 
 const map = new Map({
   target: 'map-container',
-  layers: [
-    new VectorLayer({
-      source: new VectorSource({
-        format: new GeoJSON(),
-        url: './data/countries.json',
-      }),
-    }),
-  ],
   view: new View({
     center: [0, 0],
     zoom: 2,
   }),
 });
+
+const source = new VectorSource();
+const layer = new VectorLayer({ source });
+map.addLayer(layer);
+
+map.addInteraction(new DragAndDrop({
+  source: source,
+  formatConstructors: [GeoJSON],
+}));
 
 sync(map);
