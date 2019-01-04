fa019543cc4196401f22a5d6928ef41dac448bab
rob <robertaxelkirby@gmail.com>
2019-01-04 14:52:50 +0000

    Add VectorTiles

--- main.js
+++ main.js
@@ -1,87 +1,30 @@
 import 'ol/ol.css';
-import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
-import sync from 'ol-hashed';
-import DragAndDrop from 'ol/interaction/DragAndDrop';
-import Modify from 'ol/interaction/Modify';
-import Draw from 'ol/interaction/Draw';
-import Snap from 'ol/interaction/Snap';
-import { Style, Fill, Stroke } from 'ol/style';
-import { getArea } from 'ol/sphere';
-import colormap from 'colormap';
+import MVT from 'ol/format/MVT';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import VectorTileSource from 'ol/source/VectorTile';
+
+// See https://openmaptiles.com/hosting/ for terms and access key
+const key = 'fpdB6drAGPedo6A3XDE0';
 
 const map = new Map({
   target: 'map-container',
-  view: new View({
+  view: new  View({
     center: [0, 0],
-    zoom: 2,
-  }),
+    zoom: 2
+  })
 });
 
-const source = new VectorSource({
-  format: new GeoJSON(),
-  url: './data/countries.json'
+const layer = new VectorTileLayer({
+  source: new VectorTileSource({
+    attributions: [
+      '<a href="http://www.openmaptiles.org/" target="_blank">&copy; OpenMapTiles</a>',
+      '<a href="http://www.openstreetmap.org/about/" target="_blank">&copy; OpenStreetMap contributors</a>'
+    ],
+    format: new MVT(),
+    url: `https://free-{1-3}.tilehosting.com/data/v3/{z}/{x}/{y}.pbf.pict?key=${key}`,
+    maxZoom: 14
+  })
 });
-
-const min = 1e8;
-const max = 2e13;
-const steps = 50;
-const ramp = colormap({
-  colormap: 'blackbody',
-  nshades: steps,
-});
-
-function clamp(value, low, high) {
-  return Math.max(low, Math.min(value, high));
-}
-
-function getColor(feature) {
-  const area = getArea(feature.getGeometry());
-  const f = Math.pow(clamp((area - min) / (max - min), 0, 1), 1/2);
-  const index = Math.round(f * (steps - 1));
-  return ramp[index];
-}
-
-const layer = new VectorLayer({
-  source,
-  style: function(feature) {
-    return new Style({
-      fill: new Fill({
-        color: getColor(feature),
-      }),
-      stroke: new Stroke({
-        color: 'rgba(255,255,255,0.8)',
-      }),
-    });
-  },
-});
-
 map.addLayer(layer);
-
-map.addInteraction(new DragAndDrop({
-  source: source,
-  formatConstructors: [GeoJSON],
-}));
-
-sync(map);
-
-map.addInteraction(new Modify({ source }));
-map.addInteraction(new Draw({
-  type: 'Polygon',
-  source: source,
-}));
-map.addInteraction(new Snap({ source }));
-
-const clear = document.getElementById('clear');
-clear.addEventListener('click', () => source.clear());
-
-const format = new GeoJSON({ featureProjection: 'EPSG:3857' });
-const download = document.getElementById('download');
-source.on('change', () => {
-  const features = source.getFeatures();
-  const json = format.writeFeatures(features);
-  download.href = 'data.text/json;charset=utf-8,' + json;
-});
