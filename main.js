234929c458437ccb07dfebf2b8f0762341d4ff10
rob <robertaxelkirby@gmail.com>
2019-01-04 11:35:53 +0000

    Render darkened country map

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 import 'ol/ol.css';
+import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import XYZSource from 'ol/source/XYZ';
-import { fromLonLat } from 'ol/proj';
 
-const map = new Map({
+new Map({
   target: 'map-container',
   layers: [
-    new TileLayer({
-      source: new XYZSource({
-        url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
+    new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJSON(),
+        url: './data/countries.json',
       }),
     }),
   ],
   view: new View({
     center: [0, 0],
     zoom: 2,
-  })
-})
-
-navigator.geolocation.getCurrentPosition(function(pos) {
-  const coords = fromLonLat([pos.coords.longitude, pos.coords.latitude]);
-  map.getView().animate({ center: coords, zoom: 10 });
-})
+  }),
+});
